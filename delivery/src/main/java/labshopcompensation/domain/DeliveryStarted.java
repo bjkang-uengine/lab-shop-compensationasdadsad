1d32af9288102fdc7e5349b43264ea38306b77d3
bjkang-uengine <127805649+bjkang-uengine@users.noreply.github.com>
2023-08-16 18:28:51 +0900

    commit

---
+++ delivery/src/main/java/labshopcompensation/domain/DeliveryStarted.java
@@ -0,0 +1,25 @@
+package labshopcompensation.domain;
+
+import java.time.LocalDate;
+import java.util.*;
+import labshopcompensation.domain.*;
+import labshopcompensation.infra.AbstractEvent;
+import lombok.*;
+
+@Data
+@ToString
+public class DeliveryStarted extends AbstractEvent {
+
+    private Long id;
+    private String address;
+    private String customerId;
+    private Integer quantity;
+
+    public DeliveryStarted(Delivery aggregate) {
+        super(aggregate);
+    }
+
+    public DeliveryStarted() {
+        super();
+    }
+}
